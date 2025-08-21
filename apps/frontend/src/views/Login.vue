a78ff9900e91b7dc89288fd9feb21efeacb19056
vinicius-pascoal <112434231+vinicius-pascoal@users.noreply.github.com>
2025-08-21 09:20:00 -0300

    styles updates

--- apps/frontend/src/views/Login.vue
+++ apps/frontend/src/views/Login.vue
@@ -8,38 +8,43 @@
 
     <!-- Card principal -->
     <div
-      class="relative z-10 flex w-11/12 max-w-5xl rounded-2xl bg-white/10 backdrop-blur-md shadow-lg overflow-hidden">
+      class="relative z-10 flex w-4/5 h-4/5 max-w-5xl max-h-5xl rounded-2xl bg-white/3 border-2 border-neutral-700 backdrop-blur-md shadow-lg overflow-hidden">
       <!-- Coluna Login -->
-      <div class="w-1/2 flex flex-col items-center justify-center px-10 py-12">
-        <img src="../../public/img/climbe-logo.png" alt="climb" class="h-10 mb-6" />
+      <div class=" flex flex-col items-center justify-center space-x-36-5 px-5">
+        <div>
+          <img src="../../public/img/climbe-logo.png" alt="climb" class="h-20 mb-6" />
+          <h2 class="text-white text-xl font-medium mb-6 mx-auto text-center">Portal do Sócio.</h2>
+        </div>
+        <div class="flex items-center justify-between w-full">
+          <div class="w-1/2 flex flex-col items-center">
+            <input type="email" placeholder="Digite seu email" v-model="email"
+              class="w-full h-20 px-4 py-3 mb-4 rounded-lg border border-gray-300 bg-white/20 text-white placeholder-gray-200 focus:outline-none focus:ring-2 focus:ring-cyan-400" />
 
-        <h2 class="text-white text-xl font-medium mb-6">Portal do Sócio.</h2>
-        <input type="email" placeholder="Digite seu email" v-model="email"
-          class="w-full px-4 py-3 mb-4 rounded-lg border border-gray-300 bg-white/20 text-white placeholder-gray-200 focus:outline-none focus:ring-2 focus:ring-cyan-400" />
+            <input type="password" placeholder="Digite sua senha" v-model="password"
+              class="w-full h-20 px-4 py-3 mb-6 rounded-lg border border-gray-300 bg-white/20 text-white placeholder-gray-200 focus:outline-none focus:ring-2 focus:ring-cyan-400" />
 
-        <input type="password" placeholder="Digite sua senha" v-model="password"
-          class="w-full px-4 py-3 mb-6 rounded-lg border border-gray-300 bg-white/20 text-white placeholder-gray-200 focus:outline-none focus:ring-2 focus:ring-cyan-400" />
+            <button @click="login"
+              class="w-full h-20 py-3 rounded-lg font-semibold text-white bg-neutral-500/20 hover:bg-cyan-500 transition">
+              ACESSAR
+            </button>
 
-        <button @click="login"
-          class="w-full py-3 rounded-lg font-semibold text-white bg-cyan-600 hover:bg-cyan-700 transition">
-          ACESSAR
-        </button>
+            <p class="text-white text-sm mt-4">
+              É sócio e ainda não tem acesso?
+              <a href="#" class="text-purple-400 hover:underline">Clique aqui</a>
+            </p>
+          </div>
 
-        <p class="text-white text-sm mt-4">
-          É sócio e ainda não tem acesso?
-          <a href="#" class="text-purple-400 hover:underline">Clique aqui</a>
-        </p>
-      </div>
+          <!-- Divider gradiente -->
+          <div class="w-[4px] h-4/5 bg-gradient-to-br from-cyan-400 via-transparent to-purple-400 mx-5"></div>
 
-      <!-- Divider gradiente -->
-      <div class="w-[2px] bg-gradient-to-b from-cyan-400 via-transparent to-purple-400"></div>
-
-      <!-- Coluna Texto com animação -->
-      <div class="w-1/2 flex items-center justify-center p-10">
-        <h2 class="text-white text-4xl font-bold leading-snug">
-          {{ typedText }}
-          <span class="border-r-2 border-white animate-blink ml-1"></span>
-        </h2>
+          <!-- Coluna Texto com animação -->
+          <div class="w-1/2 flex items-center justify-center p-10">
+            <h2 class="text-white text-4xl font-bold w-full h-full text-center font-bold text-6xl">
+              {{ typedText }}
+              <span class="border-r-4 border-cyan-600 animate-blink ml-1"></span>
+            </h2>
+          </div>
+        </div>
       </div>
     </div>
   </div>
